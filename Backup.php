68661215ad20241414bc2b40e7fd192ac1293dca
James Finstrom <jfinstrom@sangoma.com>
2018-07-10 22:27:20 +0000

    Fix backup class

--- Backup.php
+++ Backup.php
@@ -3,6 +3,6 @@ namespace FreePBX\modules\Phonebook;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
-    $this->addConfigs($this->FreePBX::Phonebook()->getAll());
+    $this->addConfigs($this->FreePBX->Phonebook->getAll());
   }
 }
