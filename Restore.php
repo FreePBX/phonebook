99119a306abd31ae1c8abb5886e4812ca22c2981
James Finstrom <jfinstrom@sangoma.com>
2018-07-18 21:55:15 +0000

    Clean up restore

--- Restore.php
+++ Restore.php
@@ -4,8 +4,9 @@ use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
       $configs = $this->getConfigs();
+      $pb = \FreePBX::Phonebook();
       foreach ($configs as $number => $setting) {
-        FreePBX::Phonebook()->add($number, $setting['name'], $setting['speeddial']);
+        $pb->add($number, $setting['name'], $setting['speeddial']);
       }
   }
 }
