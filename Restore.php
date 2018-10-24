c99c60e7507aad666568c20afd5df13f7935d501
James Finstrom <jfinstrom@sangoma.com>
2018-10-24 20:54:13 +0000

    Add legacy restore

--- Restore.php
+++ Restore.php
@@ -2,11 +2,21 @@
 namespace FreePBX\modules\Phonebook;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-      $configs = $this->getConfigs();
-      $pb = \FreePBX::Phonebook();
-      foreach ($configs as $number => $setting) {
-        $pb->add($number, $setting['name'], $setting['speeddial']);
-      }
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$pb = $this->FreePBX->Phonebook;
+		foreach ($configs as $number => $setting) {
+			$pb->add($number, $setting['name'], $setting['speeddial']);
+		}
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
+		$pb = $this->FreePBX->Phonebook;
+		$astdb = $this->getAstDb($tmpfiledir.'/astdb');
+		if(!isset($astdb['sysspeeddials'])){
+			return $this;
+		}
+		foreach($astdb['sysspeeddials'] as $number => $setting){
+				$pb->add($number, $setting['name'], $setting['speeddial']);
+		}
+	}
 }
